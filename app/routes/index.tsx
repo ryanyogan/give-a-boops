7a98b396e050a344952fea6a16489dd1cd6c7f4d
Ryan Yogan <ryanyogan@gmail.com>
2022-05-31 22:39:10 -0500

    boops init!

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-screen bg-slate-700 flex justify-center items-center">
+      <h2 className="text-white font-semibold text-2xl">Tailwind Works</h2>
+    </div>
+  );
+}
