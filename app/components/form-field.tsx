d81f7dcac062a4398db942ca58414e5da5505cfe
Ryan Yogan <ryanyogan@gmail.com>
2022-06-01 09:48:19 -0500

    basic login/register

---
+++ app/components/form-field.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+
+interface FormFieldProps {
+  htmlFor: string;
+  label: string;
+  type?: string;
+  value: any;
+  onChange?: (...args: any) => any;
+  error?: string;
+}
+
+export function FormField({
+  htmlFor,
+  label,
+  value,
+  type = "text",
+  onChange = () => {},
+  error = "",
+}: FormFieldProps) {
+  const [errorText, setErrorText] = useState(error);
+  useEffect(() => {
+    setErrorText(error);
+  }, [error]);
+
+  return (
+    <>
+      <label htmlFor={htmlFor} className="text-blue-600 font-semibold">
+        {label}
+      </label>
+      <input
+        name={htmlFor}
+        className="w-full p-2 rounded-xl my-2"
+        value={value}
+        onChange={(e) => {
+          onChange(e);
+          setErrorText("");
+        }}
+        type={type}
+        id={htmlFor}
+      />
+      <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
+        {errorText || ""}
+      </div>
+    </>
+  );
+}
